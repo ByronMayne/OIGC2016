242b342e4d0b403fe007154e7aac21d917deac90
Byron Mayne <Byron Mayne>
2016-04-24 11:37:05 -0400

    Added a class to view the output of the Preprocessor Directives. Renamed Divergent to Build Target

---
+++ proj.cs/OIGC2016/PreprocessorDirectives.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OIGC2016
+{
+  public class PreprocessorDirectives
+  {
+#if UNITY_EDITOR
+    public void EditorOnlyFunction()
+    {
+
+    }
+#elif UNITY_ANDROID
+    public void AndroidOnlyFunction()
+    {
+
+    }
+#elif UNITY_IOS
+    public void IOSOnlyFunction()
+    {
+
+    }
+#else
+    public void RuntimeFunction()
+    {
+
+    }
+#endif 
+  }
+}
